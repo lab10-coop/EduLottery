64d63215970bb3be23cee15da8e5cc8c64df314a
Thomas Haller <Thomas.Haller@lab10.coop>
2019-06-17 19:56:04 +0200

    removed migrations to keep the project clean.

--- deploy.js
+++ deploy.js
@@ -1,4 +1,10 @@
 
+
+//this deploy functions can be used within a truffle console.
+//truffle develop
+//or
+//truffle console --network=sigma1_rpc
+
 var lottery1 = await EduLottery.new(web3.utils.toWei('1'), '60','30')
 var lottery5 = await EduLottery.new(web3.utils.toWei('5'), '60','30')
 var lottery10 = await EduLottery.new(web3.utils.toWei('10'), '60','30')
